f1f758199135b4a60354bf67b312aace4b28804d
Mark Bassett <bassmarkie@gmail.com>
2018-09-17 11:42:33 -0500

    with first test specs

--- bubblesort.js
+++ bubblesort.js
@@ -1,8 +1,10 @@
 const bubbleSort = (array) => {
+  // let swapCount = 0
   for (let i = 0; i < array.length - 1; i++) {
     for (let j = 0 ; j < array.length - i; j++) {
       if (array[j-1] > array[j]) {
         swap(array, j-1, j)
+        // swapCount++;
       }
     }
   }
@@ -13,5 +15,6 @@ function swap(array, a, b) {
   let temp = array[a];
   array[a] = array[b];
   array[b] = temp;
+
 }
 
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,8 +2,8 @@ describe('Bubble Sort', function(){
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
-});
+  it('handles an simple integer array', function(){
+    expect( bubbleSort([6,2,7,9,1]) ).toEqual( [1,2,6,7,9] );
+  });
 
-beforeAll(function () {
-  spyOn(bubbleSort(array)).and.callThrough();
 });
